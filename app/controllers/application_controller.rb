004fa0b19a2db040e43c34123072f78c0f12b535
Max Ono <ono.m@husky.neu.edu>
2019-08-30 14:32:46 -0700

    [FEAT] Require auth; Add devise layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
+
   def current_day
     Date.today.strftime('%A').downcase
   end
   helper_method :current_day
+
+  private
+
+  def layout_by_resource
+    return 'devise' if devise_controller?
+
+    'application'
+  end
 end
